b91a6fee12f9e9e03a722dc1e7c175a463fdfa93
Hubert Olender <olender.hub@gmail.com>
2015-06-16 21:23:32 +0200

    Clean application from useless error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActionController::RoutingError, :with => :render_not_found
-  rescue_from StandardError, :with => :render_server_error
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -9,13 +7,4 @@ class ApplicationController < ActionController::Base
   	flash[:warning] = ex.message
   	redirect_to root_url
   end
-
-  protected
-    def render_not_found
-      render "shared/404", :status => 404
-    end
-
-    def render_server_error
-      render "shared/500", :status => 500
-    end
 end
